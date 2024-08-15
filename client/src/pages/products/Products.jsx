a7a02f1f5c12e41a5d035ba572aaa3e1804adf0c
kaziabulhasib <hasibkazi420@gmail.com>
2024-08-15 11:18:41 +0530

    feat: product data fetchng

--- client/src/pages/products/Products.jsx
+++ client/src/pages/products/Products.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Products = () => {
+  const url = "http://localhost:5000/products";
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setProducts(data);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>This is products</h1>
+      <h1>This is products : {products.length}</h1>
     </div>
   );
 };
